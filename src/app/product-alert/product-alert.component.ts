9e18dfadd3c2d10fbf69ee67ab76bdd89d34aaab
Anshu-17 <anshurp007@gmail.com>
2021-02-10 00:23:04 +0530

    angular commit 1

---
+++ src/app/product-alert/product-alert.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-product-alert",
+  templateUrl: "./product-alert.component.html",
+  styleUrls: ["./product-alert.component.css"]
+})
+export class ProductAlertComponent implements OnInit {
+  @Input() productInner;
+  constructor() {}
+
+  ngOnInit() {}
+}
